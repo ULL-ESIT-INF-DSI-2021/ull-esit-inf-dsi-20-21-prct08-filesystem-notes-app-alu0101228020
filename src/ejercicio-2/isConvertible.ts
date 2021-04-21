beaccbd68f1edb9640faaee76099b408b87c828e
Dayana Armas <alu0101228020@ull.edu.es>
2021-04-21 23:54:19 +0100

    Adding changes

--- src/ejercicio-2/isConvertible.ts
+++
@@ -1,9 +0,0 @@
-export interface isConvertible<T> {
-    valueActual: number;
-    valueUnitsActual: T;
-    /**
-     * Method that calculates the conversion
-     * @param desiredUnit Measure to want to convert
-     */
-    convert(desiredUnit : T): number;
-}
